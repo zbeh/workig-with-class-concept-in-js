d7c94d7131d0eb230e5398c24580675d06962070
zi <beheshtizade275@gmail.com>
2021-12-20 11:08:31 +0330

    1st question 2nd part

---
+++ 1/1-b.js
@@ -0,0 +1,31 @@
+class Common{
+    constructor(className, classUnit, classCapacity){
+        this.className = className
+        this.classUnit = classUnit
+        this.classCapacity = classCapacity
+    }
+}
+class Soft extends Common{
+    constructor(className , classUnit, classCapacity, book){
+        super(className,classUnit, classCapacity)
+        this.book = book
+    }
+    getFullcourse(){
+        console.log(`className:${this.className} classUnit:${this.classUnit} classCapacity:${this.classCapacity} book:${this.book}`)
+    }
+}
+class Network extends Common{
+    constructor(className , classUnit, classCapacity, project){
+        super(className,classUnit, classCapacity)
+        this.project = project
+    }
+    getFullcourse(){
+        console.log(`className:${this.className} classUnit:${this.classUnit} classCapacity:${this.classCapacity} project:${this.project}`)
+    }
+}
+const software = new Soft("softEngineering", 3 , 50 , "Database")
+console.log(software)
+software.getFullcourse()
+const net = new Network("network" , 3,20, true)
+console.log(net)
+net.getFullcourse()
