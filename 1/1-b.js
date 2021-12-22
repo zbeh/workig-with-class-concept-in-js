958fb58e5996d0b5b8ae28fdf7f981cd3e8352d3
zi <beheshtizade275@gmail.com>
2021-12-22 16:27:03 +0330

    editing the 1st question

--- 1/1-b.js
+++ 1/1-b.js
@@ -1,31 +1,43 @@
-class Common{
-    constructor(className, classUnit, classCapacity){
-        this.className = className
-        this.classUnit = classUnit
-        this.classCapacity = classCapacity
-    }
-}
-class Soft extends Common{
-    constructor(className , classUnit, classCapacity, book){
-        super(className,classUnit, classCapacity)
-        this.book = book
-    }
-    getFullcourse(){
-        console.log(`className:${this.className} classUnit:${this.classUnit} classCapacity:${this.classCapacity} book:${this.book}`)
-    }
-}
-class Network extends Common{
-    constructor(className , classUnit, classCapacity, project){
-        super(className,classUnit, classCapacity)
-        this.project = project
-    }
-    getFullcourse(){
-        console.log(`className:${this.className} classUnit:${this.classUnit} classCapacity:${this.classCapacity} project:${this.project}`)
-    }
-}
-const software = new Soft("softEngineering", 3 , 50 , "Database")
+// class Common{
+//     constructor(className, classUnit, classCapacity){
+//         this.className = className
+//         this.classUnit = classUnit
+//         this.classCapacity = classCapacity
+//     }
+// }
+// class Soft extends Common{
+//     constructor(className , classUnit, classCapacity, book){
+//         super(className,classUnit, classCapacity)
+//         this.book = book
+//     }
+//     getFullcourse(){
+//         console.log(`className:${this.className} classUnit:${this.classUnit} classCapacity:${this.classCapacity} book:${this.book}`)
+//     }
+// }
+// class Network extends Common{
+//     constructor(className , classUnit, classCapacity, project){
+//         super(className,classUnit, classCapacity)
+//         this.project = project
+//     }
+//     getFullcourse(){
+//         console.log(`className:${this.className} classUnit:${this.classUnit} classCapacity:${this.classCapacity} project:${this.project}`)
+//     }
+// }
+// const software = new Soft("softEngineering", 3 , 50 , "Database")
+// console.log(software)
+// software.getFullcourse()
+// const net = new Network("network" , 3,20, true)
+// console.log(net)
+// net.getFullcourse()
+
+function Subject(className , classUnit, classCapacity){
+    this.className = className
+    this.classUnit = classUnit
+    this.classCapacity = classCapacity
+}    
+const software = new Subject("softEngineering",3,50)
+software.book = "database"
 console.log(software)
-software.getFullcourse()
-const net = new Network("network" , 3,20, true)
+const net = new Subject("network", 3 ,30)
+net.project = true
 console.log(net)
-net.getFullcourse()
--- 1/1.js
+++ 1/1.js
@@ -1,13 +1,11 @@
-class Subject{
-    constructor(className , classUnit, classCapacity){
-     this.className = className
-     this.classUnit = classUnit
-     this.classCapacity = classCapacity
+function Subject(className , classUnit, classCapacity){
+    this.className = className
+    this.classUnit = classUnit
+    this.classCapacity = classCapacity
+    this.getFullcourse = function(){
+     console.log(`className:${ this.className},classUnit:${this.classUnit},classCapacity:${this.classCapacity}`)
     }
-    getFullcourse (){
-        console.log("className:"+ this.className+" " +"calssUnit:"+ this.classUnit+" "+ "classCapacity:"+this.classCapacity)
-    }
-}
+}    
 const soft = new Subject("softEngineering",3,50)
 soft.getFullcourse()
 const network = new Subject("network", 3 ,30)
